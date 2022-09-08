c136b27f4e94d4fc59e49e2c174347ec109582a3
jaeh <jascha@jaeh.at>
2022-09-08 15:03:46 +0200

    src/index: use an object instead of two strings to allow multicharacter replacements

--- src/index.mjs
+++ src/index.mjs
@@ -3,48 +3,69 @@ export const slugify = (str, dash = false) => {
     return ''
   }
 
-  str = str.replace(/^\s+|\s+$/g, '')
-
-  str = str.toLowerCase()
-
-  // Remove accents, swap ñ for n, etc
-  const fromString =
-    'ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđa·/_,:;'
-
-  const fromArray = fromString.split('')
-
-  const toString =
-    'AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDda------'
-  const toArray = toString.split('')
-
-  for (let i = 0; i < fromArray.length; i++) {
-    let to = toString.charAt(i)
-    const fr = fromString.charAt(i)
-
-    if (fr === 'Ä') {
-      to = 'Ae'
-    } else if (fr === 'Ö') {
-      to = 'Oe'
-    } else if (fr === 'Ü') {
-      to = 'Ue'
-    } else if (fr === 'ä') {
-      to = 'ae'
-    } else if (fr === 'ö') {
-      to = 'oe'
-    } else if (fr === 'ü') {
-      to = 'ue'
-    }
-
-    str = str.replace(new RegExp(fr, 'g'), to)
+  const replaceMap = {
+    A: 'ÁÂÀÃÅĀ',
+    B: 'Þ',
+    C: 'ČÇĆ',
+    D: 'ĎĐ',
+    E: 'ÉĚËÈÊẼĔȆ',
+    I: 'ÍÌÎÏ',
+    N: 'ŇÑ',
+    O: 'ÓÒÔÕØ',
+    R: 'ŘŔ',
+    S: 'ŠŞ',
+    T: 'Ť',
+    U: 'ÚŮÙÛ',
+    Y: 'ÝŸ',
+    Z: 'Ž',
+    a: 'áâàãāầảàå',
+    b: 'þ',
+    c: 'čçć',
+    d: 'ďđ',
+    e: 'éěëèêẽĕȇ',
+    h: 'ḩ',
+    i: 'íìîïī',
+    n: 'ňñ',
+    o: 'óòôõøộðồơ',
+    r: 'řŕ',
+    s: 'šş',
+    ss: 'ß',
+    t: 'ť',
+    u: 'úůùû',
+    y: 'ýÿ',
+    z: 'ž',
+    Ae: 'Ä',
+    Oe: 'Ö',
+    Ue: 'Ü',
+    ae: 'ä',
+    oe: 'ö',
+    ue: 'ü',
+    '-': '·/ _,: ; ',
   }
 
-  // Remove invalid chars
-  str = str
+  const strArray = str.split('')
+  const entries = Object.entries(replaceMap)
+
+  for (let i = 0; i < strArray.length; i++) {
+    const char = strArray[i]
+
+    const match = entries.find(([_,val]) => val.includes(char))
+
+    if (match) {
+      const [k] = match
+      strArray[i] = k
+    }
+  }
+
+  // Make sure we remove all invalid chars
+  const final = strArray
+    .join('')
+    .toLowerCase()
     .replace(/[^a-z0-9 -]/g, '')
     .replace(/\s+/g, dash ? '-' : '')
     .replace(/-+/g, dash ? '-' : '')
 
-  return str
+  return final
 }
 
 export default slugify
